090548805813556ef9f59fad7fa704a3e5693f73
JUN <chltmdwns96@naver.com>
2023-05-20 21:11:13 +0900

    project init

---
+++ src/main/java/sopt/sopkathon/soptkathon9th/Soptkathon9thApplication.java
@@ -0,0 +1,13 @@
+package sopt.sopkathon.soptkathon9th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soptkathon9thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soptkathon9thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/sopkathon/soptkathon9th/Soptkathon9thApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.sopkathon.soptkathon9th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soptkathon9thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
